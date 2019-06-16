c8521b9c063bb9ae5cadf283f3674eacb95b31c8
Mohammed Al-Khalifah <mohak2009@hotmail.com>
2019-06-16 22:25:32 +0300

    fifth

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -10,10 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int x;
-
-        int y, z;
-
+        int mm;
 
     }
 }
